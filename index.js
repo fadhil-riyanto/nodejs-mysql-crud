c2ff1e170d0d6b19f5ee008b194de0efa9e4ec05
fadhil-riyanto <id.fadhil.riyanto@gmail.com>
2020-12-03 14:54:13 +0700

    change root pass database

--- index.js
+++ index.js
@@ -9,70 +9,70 @@ const bodyParser = require('body-parser');
 //use mysql database
 const mysql = require('mysql');
 const app = express();
- 
+
 //konfigurasi koneksi
 const conn = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'nodejs-crud'
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'nodejs-crud'
 });
- 
+
 //connect ke database
-conn.connect((err) =>{
-  if(err) throw err;
-  console.log('Mysql Connected...');
+conn.connect((err) => {
+    if (err) throw err;
+    console.log('Mysql Connected...');
 });
- 
+
 //set views file
-app.set('views',path.join(__dirname,'views'));
+app.set('views', path.join(__dirname, 'views'));
 //set view engine
 app.set('view engine', 'hbs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 //set folder public sebagai static folder untuk static file
-app.use('/assets',express.static(__dirname + '/public'));
- 
+app.use('/assets', express.static(__dirname + '/public'));
+
 //route untuk homepage
-app.get('/',(req, res) => {
-  let sql = "SELECT * FROM product";
-  let query = conn.query(sql, (err, results) => {
-    if(err) throw err;
-    res.render('product_view',{
-      results: results
-    });
-  });
+app.get('/', (req, res) => {
+    let sql = "SELECT * FROM product";
+    let query = conn.query(sql, (err, results) => {
+        if (err) throw err;
+        res.render('product_view', {
+            results: results
+        });
+    });
 });
- 
+
 //route untuk insert data
-app.post('/save',(req, res) => {
-  let data = {product_name: req.body.product_name, product_price: req.body.product_price};
-  let sql = "INSERT INTO product SET ?";
-  let query = conn.query(sql, data,(err, results) => {
-    if(err) throw err;
-    res.redirect('/');
-  });
+app.post('/save', (req, res) => {
+    let data = { product_name: req.body.product_name, product_price: req.body.product_price };
+    let sql = "INSERT INTO product SET ?";
+    let query = conn.query(sql, data, (err, results) => {
+        if (err) throw err;
+        res.redirect('/');
+    });
 });
- 
+
 //route untuk update data
-app.post('/update',(req, res) => {
-  let sql = "UPDATE product SET product_name='"+req.body.product_name+"', product_price='"+req.body.product_price+"' WHERE product_id="+req.body.id;
-  let query = conn.query(sql, (err, results) => {
-    if(err) throw err;
-    res.redirect('/');
-  });
+app.post('/update', (req, res) => {
+    let sql = "UPDATE product SET product_name='" + req.body.product_name + "', product_price='" + req.body.product_price + "' WHERE product_id=" + req.body.id;
+    let query = conn.query(sql, (err, results) => {
+        if (err) throw err;
+        res.redirect('/');
+    });
 });
- 
+
 //route untuk delete data
-app.post('/delete',(req, res) => {
-  let sql = "DELETE FROM product WHERE product_id="+req.body.product_id+"";
-  let query = conn.query(sql, (err, results) => {
-    if(err) throw err;
-      res.redirect('/');
-  });
+app.post('/delete', (req, res) => {
+    let sql = "DELETE FROM product WHERE product_id=" + req.body.product_id + "";
+    let query = conn.query(sql, (err, results) => {
+        if (err) throw err;
+        res.redirect('/');
+    });
 });
- 
+
 //server listening
 app.listen(8000, () => {
-  console.log('Server is running at port 8000');
+    console.log('Server is running at port 8000');
 });
